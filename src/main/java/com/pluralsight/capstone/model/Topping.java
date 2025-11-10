c3f8bf2ff36d26944e2ab1c6cdd04331f032368e
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-10 14:35:45 -0500

    This a constructor for the Toppiong class in Java. It initializes a new Topping object with two parameters. Name, a string representing the topping name, for example, Pepperoni or Mushrooms. Second is category, a string representing the topping type, example, Meat or Regular.

--- src/main/java/com/pluralsight/capstone/model/Topping.java
+++ src/main/java/com/pluralsight/capstone/model/Topping.java
@@ -3,4 +3,12 @@ package com.pluralsight.capstone.model;
 // Abstract Topping
 
 public abstract class Topping {
+    private String name;
+    private String category;
+    private int extraAmount = 0;
+    
+    public Topping(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
 }
