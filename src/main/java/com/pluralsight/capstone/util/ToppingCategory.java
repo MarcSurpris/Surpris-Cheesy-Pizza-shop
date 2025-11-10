160831a79c9745359e5471adbafc2b93bf3b95ae
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-10 11:50:03 -0500

    The ToppingCategory class defines public static final String constants to categorize pizza toppings.

--- src/main/java/com/pluralsight/capstone/util/ToppingCategory.java
+++ src/main/java/com/pluralsight/capstone/util/ToppingCategory.java
@@ -2,4 +2,9 @@ package com.pluralsight.capstone.util;
 
 // ToppingCategory class with constants
 public class ToppingCategory {
+    public static final String Meat = "MEAT";
+    public static final String CHEESE = "CHEESE";
+    public static final String REGULAR = "REGULAR";
+    public static final String SAUCE = "SAUCE";
+    public static final String SIDE = "SIDE";
 }
