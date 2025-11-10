0ef7c81d71c4d85314e3f1ef697822d140f47c6b
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-10 10:01:10 -0500

    created class PizzaSize.java

--- src/main/java/com/pluralsight/capstone/util/PizzaSize.java
+++ src/main/java/com/pluralsight/capstone/util/PizzaSize.java
@@ -1,4 +1,9 @@
 package com.pluralsight.capstone.util;
 
 public class PizzaSize {
+    public static final String PERSONAL_8_INCH = "PERSONAL_8_INCH";
+    public static final String MEDIUM_12_INCH = "MEDIUM_12_INCH";
+    public static final String LARGE_16_INCH = "LARGE_16_INCH";
+
+
 }
