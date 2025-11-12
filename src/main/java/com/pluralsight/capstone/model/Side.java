eaffae8c8d8e775c8ecb4c3cbd5714893f040774
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-12 11:31:03 -0500

    This method overrides getBasePrice(String size) from the Topping class in the Side subclass.

--- src/main/java/com/pluralsight/capstone/model/Side.java
+++ src/main/java/com/pluralsight/capstone/model/Side.java
@@ -7,4 +7,9 @@ public class Side extends Topping {
     public Side(String name) {
         super(name, ToppingCategory.SIDE);
     }
+
+    @Override
+    public double getBasePrice(String size) {
+        return 0.0;
+    }
 }
