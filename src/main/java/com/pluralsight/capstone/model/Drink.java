7329f23f4075524022c28474c756e05202ae1ebe
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 15:22:50 -0500

    These lines define private fields and a constructor in the Drink class, which implements Item.

--- src/main/java/com/pluralsight/capstone/model/Drink.java
+++ src/main/java/com/pluralsight/capstone/model/Drink.java
@@ -1,4 +1,11 @@
 package com.pluralsight.capstone.model;
 // Drink
 public class Drink implements Item {
+    private String drinkSize;
+    private String flavor;
+
+    public Drink(String drinkSize, String flavor){
+        this.drinkSize = drinkSize;
+        this.flavor = flavor;
+    }
 }
