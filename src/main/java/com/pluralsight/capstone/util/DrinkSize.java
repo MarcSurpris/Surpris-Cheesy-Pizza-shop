a8b0220fc499453d89669004180ac38fadd9c3bc
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 11:56:24 -0500

    This Java method getPrice takes a String parameter dSize, short for drink size. It returns a double representing the base price based on size.

--- src/main/java/com/pluralsight/capstone/util/DrinkSize.java
+++ src/main/java/com/pluralsight/capstone/util/DrinkSize.java
@@ -7,5 +7,12 @@ public class DrinkSize {
     public static final String MEDIUM = "MEDIUM";
     public static final String LARGE = "LARGE";
 
-
+    public static double getPrice(String dSize) {
+        return switch (dSize) {
+            case SMALL -> 2.00;
+            case MEDIUM -> 2.50;
+            case LARGE -> 3.00;
+            default -> 0.0;
+        };
+    }
 }
