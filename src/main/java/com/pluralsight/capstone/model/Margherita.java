f19338b2e67b284693cad721532e8af0de859b58
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 11:10:20 -0500

    This the default constructor for the Margherita class (a subclass of Pizza). Creates a pre-configured Margherita pizza with standard defaults.

--- src/main/java/com/pluralsight/capstone/model/Margherita.java
+++ src/main/java/com/pluralsight/capstone/model/Margherita.java
@@ -1,4 +1,15 @@
 package com.pluralsight.capstone.model;
 
+import com.pluralsight.capstone.util.CrustType;
+import com.pluralsight.capstone.util.PizzaSize;
+
 public class Margherita extends Pizza {
+    public Margherita() {
+        super(PizzaSize.MEDIUM_12_INCH, CrustType.REGULAR);
+        addTopping(new Cheese("Mozzarella"));
+        addTopping(new RegularTopping("Tomatoes"));
+        addTopping(new RegularTopping("Basil"));
+        addTopping(new Sauce("Marinara"));
+        addTopping(new Sauce("Olive Oil"));
+    }
 }
